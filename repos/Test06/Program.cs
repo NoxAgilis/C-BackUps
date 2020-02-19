60937de9ca73090a765a0e7a5478c92e979d681d
NoxAgilis <jesusruilu@gmail.com>
2020-02-19 13:56:39 +0100

    Test bucle for

---
+++ repos/Test06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nº 1: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Nº 2: ");
+            int y = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= y; i++ )
+            {
+                Console.WriteLine(x*i);
+            }
+        }
+    }
+}
