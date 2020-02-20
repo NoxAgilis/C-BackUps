eedced4ae3a57b4e83ca9a7caf5144b803819d3a
NoxAgilis <jessuruilu@gmail.com>
2020-02-20 13:56:12 +0100

    Error array

---
+++ repos/Test10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuantos números vas a introducir? ");
+            int l = int.Parse(Console.ReadLine());
+            int[] list01 = new int[l];
+
+            for (int i = 0; i < list01.Length; i++) 
+            {
+                Console.Write("Dime un número: " + i);
+                list01[i] = int.Parse(Console.ReadLine());
+            }
+            int[] list02 = new int[list01.Length];
+
+            for (int i = 0; i < list02.Length; i++)
+            {
+                list02[i] = list01[i];
+                Console.WriteLine(list02[i]);
+            }
+        }
+    }
+}
