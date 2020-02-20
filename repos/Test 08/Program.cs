62f18d63ea6cd5907c9775a8d87b249f5227cbc1
NoxAgilis <jessuruilu@gmail.com>
2020-02-20 11:21:28 +0100

    Suma consecutiva

---
+++ repos/Test 08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Test08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.Write("¿Que número sumo? ");
+                int x = int.Parse(Console.ReadLine());
+                y += x;
+            }
+            Console.WriteLine(y);
+        }
+    }
+}
