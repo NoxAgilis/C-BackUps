6692a03945704cef137ef1f888993b7ea0744ef1
NoxAgilis <jessuruilu@gmail.com>
2020-02-20 13:29:55 +0100

    Arrays, como se usan y como se pueden usar

--- repos/Test09/Program.cs
+++ repos/Test09/Program.cs
@@ -6,7 +6,24 @@ namespace Test09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("¿Cuantos números vas a introducir? ");
+            int l = int.Parse(Console.ReadLine());
+            int[] list = new int[l];
+            
+            for (int i = 0; i < list.Length; i++) 
+            {
+                Console.Write("Dime un número: " +
+                    "");
+                list[i] = int.Parse(Console.ReadLine());
+            }
+            int r = 0;
+
+            for (int i = 0; i < list.Length; i++) 
+            {
+                r += list[i];
+            }
+            Console.WriteLine("El resultado es " + r);
+
         }
     }
 }
