55365da2d39ef9cf950122ae144304a8f393266b
NoxAgilis <jesusruilu@gmail.com>
2020-03-10 12:09:38 +0100

    Cambio de pc

---
+++ repos/Test15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Test15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuantos números vas a introducir? ");
+            int l = int.Parse(Console.ReadLine());
+            int[] list = new int[l];
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write("Dime un número: " + "");
+                list[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int number in list)
+                {
+                int c = 0;
+                int f = 0;
+                if (c < list.Length) ;
+                    {
+                    int t = c++;
+                    if (list[c] == list[t])
+                        {
+                        t++;
+                        }
+                    Console.WriteLine("Se repiten: " +t);
+                    }
+                }
+        }
+    }
+}
---
+++ repos/Test17/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Test17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuantas filas quieres? ");
+            int z = int.Parse(Console.ReadLine());
+            for (int i = 1; i < z; i++)
+            {
+                for (int t = 0; t < i; t++)
+                {
+                    Console.Write("W ");
+                }
+                Console.WriteLine();
+            }            
+            for (int i = 1; i < z; i++)
+            {
+                int a = 1;
+                for (int t = 0; t < i; t++)
+                {
+                   
+                    Console.Write(+a+ " ");
+                    a++;
+
+                }
+                Console.WriteLine();
+            }
+            for (int i = 1; i < z; i++)
+            {
+                for (int t = 0; t < i; t++)
+                {
+                    Console.Write(+i+ " ");
+                }
+                Console.WriteLine();
+            }
+            int b = 1;
+            for (int i = 1; i < z; i++)
+            {
+
+                for (int t = 0; t < i; t++)
+                {
+                    Console.Write(b+ " ");
+                    b++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
