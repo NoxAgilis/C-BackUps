fa28ec7ed9d4e5d34e8b5e78367b0b02d910f604
NoxAgilis <jesusruilu@gmail.com>
2020-03-11 13:57:10 +0100

    Mama, que estoy haciendo funciones

---
+++ repos/Test18/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> x = new List<int> ();
+            //Console.Write("¿La longitud de la lista? ");
+            //int l = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Dime los números ");
+            //for (int i = 0; i < l; i++)
+            //{
+            //    x.Add(int.Parse(Console.ReadLine()));
+            //}
+            //for (int i = x.Count - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(x[i]);
+            //}
+            //List<int> x = new List<int>();
+            //Console.Write("¿Cuantos números van a ser? ");
+            //int l = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < l; i++)
+            //{
+            //    x.Add(int.Parse(Console.ReadLine()));
+            //}            
+            //for (int i = 0; i < x.Count; i++)
+            //{
+            //    for (int j = i + 1; j < x.Count; j++)
+            //    {
+            //        if (x[j] < x[i])
+            //        {
+            //            int aux = x[i];
+            //            x[i] = x[j];
+            //            x[j] = aux;
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < x.Count; i++)
+            //{
+            //    Console.WriteLine(x[i]);
+            //}
+            int x = 0;
+            do
+            {
+                Console.WriteLine("Con el 1 sumas, el 2 resta, el 3 multiplica, el 4 divide y el 5 cierra, ¿cual quieres? ");
+                x = int.Parse(Console.ReadLine());
+                switch (x)
+                {
+                    case 1:
+                        Console.Write("Dime el primer número: ");
+                        int a = int.Parse(Console.ReadLine());
+                        Console.Write("Dime el segundo número: ");
+                        int b = int.Parse(Console.ReadLine());
+                        Console.WriteLine((a + b));
+                        break;
+                    case 2:
+                        Console.Write("Dime el primer número: ");
+                        int c = int.Parse(Console.ReadLine());
+                        Console.Write("Dime el segundo número: ");
+                        int d = int.Parse(Console.ReadLine());
+                        Console.WriteLine((c - d));
+
+                        break;
+                    case 3:
+                        Console.Write("Dime el primer número: ");
+                        int e = int.Parse(Console.ReadLine());
+                        Console.Write("Dime el segundo número: ");
+                        int f = int.Parse(Console.ReadLine());
+                        Console.WriteLine((e * f));
+                        break;
+                    case 4:
+                        Console.Write("Dime el primer número: ");
+                        int g = int.Parse(Console.ReadLine());
+                        Console.Write("Dime el segundo número: ");
+                        int h = int.Parse(Console.ReadLine());
+                        Console.WriteLine((g / h));
+                        break;
+                    case 5:
+                        Console.WriteLine("Adios");
+                        break;
+                    default:
+                        Console.WriteLine("Eso no");
+                        break;
+                }
+            } while (x != 5);
+        }
+    }
+}
---
+++ repos/Test19/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test19
+{
+    class Program
+    {
+        static int Quest(string text)
+        {
+            Console.Write(text);
+            return int.Parse(Console.ReadLine());
+        }
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static int Res(int x, int y)
+        {
+            return x - y;
+        }
+        static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = Quest("¿El primer número? ");
+            int y = Quest("¿El segundo número? ");
+            int s = 0;
+            do
+            {
+                 s = Quest("Con el 1 sumas, el 2 resta, el 3 multiplica, el 4 divide y el 5 cierra, ¿cual quieres? ");
+
+                switch (s)
+                {
+                    case 1:
+                        Console.WriteLine(Sum(x, y));
+                        break;
+                    case 2:
+                        Console.WriteLine(Res(x, y));
+                        break;
+                    case 3:
+                        Console.WriteLine(Mul(x, y));
+                        break;
+                    case 4:
+                        Console.WriteLine(Div(x, y));
+                        break;
+                    case 5:
+                        Console.WriteLine("Adios");
+                        break;
+                    default:
+                        Console.WriteLine("Eso no");
+                        break;
+                }
+            } while (s  != 5);
+        }
+    }
+}
